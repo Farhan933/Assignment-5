79c45e38184096174c81b05c8550a7d41574c4f8
Farhan933 <farhanfarabiemon@gmail.com>
2023-02-21 20:07:41 +0600

    approximate ok

--- js/basic.js
+++ js/basic.js
@@ -1,1 +1,54 @@
-console.log('hello, javaScript')
+// Dane van Niekerk
+document.getElementById('Dane-van-Niekerk').addEventListener('click', function(){
+    let findFirstName         = getAllTheNameById('first-name');
+    let findTheNameYouReplace = setTheValueById('first-item', findFirstName);
+});
+
+// Sana Mir
+document.getElementById('Sana Mir').addEventListener('click', function(){
+    let findSecondName        = getAllTheNameById('second-name');
+    let findTheNameYouReplace = setTheValueById('second-item', findSecondName);
+});
+
+// Charlotte Edwards
+document.getElementById('Charlotte Edwards').addEventListener('click', function(){
+    let findThirdName         = getAllTheNameById('third-name');
+    let findTheNameYouReplace = setTheValueById('third-item', findThirdName);
+});
+
+// Ellyse perry
+document.getElementById('Ellyse perry').addEventListener('click', function(){
+    let findFourthName        = getAllTheNameById('fourth-name');
+    let findTheNameYouReplace = setTheValueById('fourth-item', findFourthName);
+});
+
+// Frederque Overdijk
+document.getElementById('Frederque Overdijk').addEventListener('click', function(){
+    let findFifthName         = getAllTheNameById('fifth-name');
+    let findTheNameYouReplace = setTheValueById('fifth-item', findFifthName);
+});
+
+//                      ... Budget card... 
+
+// Calculate Player Expenses
+document.getElementById('selected-player-cost').addEventListener('click', function(){
+    let getTheInputValue = getAllInputField('selected-input-field');
+    let Expenses           = getTheInputValue * 5;
+    let findTheExpense     = getAllTheNameById('expenses');
+    let setTheExpenseValue = setTheValueById('expenses', Expenses);
+});
+
+// Calculate Total
+document.getElementById('calculate-total').addEventListener('click', function(){
+    let getTheInputValue = getAllInputField('selected-input-field');
+    let Expenses           = getTheInputValue * 5;
+
+    let findTheManagerCost =  getAllInputField('manager-cost');
+    let findTheCoachCost   =  getAllInputField('coach-cost');
+
+    ManagerAndCoachTotal   = findTheManagerCost + findTheCoachCost;
+    TotalCost              = Expenses + ManagerAndCoachTotal ;
+
+    let setTheTotalCost    = setTheValueById('total-expense', TotalCost);
+
+});
--- js/common.js
+++ js/common.js
@@ -1,1 +1,20 @@
-console.log('hello, common.Js')
+// For get the Function
+function getAllTheNameById(enterYourId){
+    let findTheName = document.getElementById(enterYourId);
+    let findTheValue = findTheName.innerText;
+    return findTheValue;
+}
+
+// For set the Function
+function setTheValueById(element, value){
+    let findId = document.getElementById(element);
+    findId.innerText = value;
+}
+
+// For get all input field
+function getAllInputField(enterYourId){
+    let findTheId = document.getElementById(enterYourId);
+    let findTheValueOfId = findTheId.value;
+    let convertIntoNumber = parseFloat(findTheValueOfId);
+    return convertIntoNumber;
+}
