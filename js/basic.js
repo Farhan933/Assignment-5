283d5b5b31926db0ad873e674034d24bf462e80c
Farhan933 <farhanfarabiemon@gmail.com>
2023-02-22 19:34:37 +0600

    so close

--- js/basic.js
+++ js/basic.js
@@ -1,31 +1,73 @@
 // Dane van Niekerk
 document.getElementById('Dane-van-Niekerk').addEventListener('click', function(){
-    let findFirstName         = getAllTheNameById('first-name');
-    let findTheNameYouReplace = setTheValueById('first-item', findFirstName);
+    let parent      = document.getElementById('parent');
+    let createChild = document.createElement('li');
+        createChild.innerText = 'Dane-van-Niekerk';
+    parent.appendChild(createChild).style.paddingBottom = '5px';
+
+// For disable the button 
+    let disableTheButton = document.getElementById('Dane-van-Niekerk');
+    disableTheButton.setAttribute('disabled', true);
 });
 
 // Sana Mir
 document.getElementById('Sana Mir').addEventListener('click', function(){
-    let findSecondName        = getAllTheNameById('second-name');
-    let findTheNameYouReplace = setTheValueById('second-item', findSecondName);
+    let parent      = document.getElementById('parent');
+    let createChild = document.createElement('li');
+        createChild.innerText = 'Sana Mir';
+    parent.appendChild(createChild).style.paddingBottom = '5px';
+
+    // For disable the button 
+    let disableTheButton = document.getElementById('Sana Mir');
+    disableTheButton.setAttribute('disabled', true);
 });
 
 // Charlotte Edwards
 document.getElementById('Charlotte Edwards').addEventListener('click', function(){
-    let findThirdName         = getAllTheNameById('third-name');
-    let findTheNameYouReplace = setTheValueById('third-item', findThirdName);
+    let parent      = document.getElementById('parent');
+    let createChild = document.createElement('li');
+        createChild.innerText = 'Charlotte Edwards';
+    parent.appendChild(createChild).style.paddingBottom = '5px';
+
+    // For disable the button 
+    let disableTheButton = document.getElementById('Charlotte Edwards');
+    disableTheButton.setAttribute('disabled', true);
 });
 
 // Ellyse perry
 document.getElementById('Ellyse perry').addEventListener('click', function(){
-    let findFourthName        = getAllTheNameById('fourth-name');
-    let findTheNameYouReplace = setTheValueById('fourth-item', findFourthName);
+    let parent      = document.getElementById('parent');
+    let createChild = document.createElement('li');
+        createChild.innerText = 'Ellyse perry';
+    parent.appendChild(createChild).style.paddingBottom = '5px';
+    
+    // For disable the button 
+    let disableTheButton = document.getElementById('Ellyse perry');
+    disableTheButton.setAttribute('disabled', true);
 });
 
 // Frederque Overdijk
 document.getElementById('Frederque Overdijk').addEventListener('click', function(){
-    let findFifthName         = getAllTheNameById('fifth-name');
-    let findTheNameYouReplace = setTheValueById('fifth-item', findFifthName);
+    let parent      = document.getElementById('parent');
+    let createChild = document.createElement('li');
+        createChild.innerText = 'Frederque Overdijk';
+    parent.appendChild(createChild).style.paddingBottom = '5px';
+    
+    // For disable the button 
+    let disableTheButton = document.getElementById('Frederque Overdijk');
+    disableTheButton.setAttribute('disabled', true);
+});
+
+// Sarah Tailor
+document.getElementById('Sarah Taylor').addEventListener('click', function(){
+    let parent      = document.getElementById('parent');
+    let createChild = document.createElement('li');
+        createChild.innerText = 'Sarah Taylor';
+    parent.appendChild(createChild).style.paddingBottom = '5px';
+    
+    // For disable the button 
+    let disableTheButton = document.getElementById('Sarah Taylor');
+    disableTheButton.setAttribute('disabled', true);
 });
 
 //                      ... Budget card... 
